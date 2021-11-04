d6e4b7ba72819b13a67d387afbf3efcc7da7c074
Tania Tasmin <taniat.kaau@email.com>
2021-11-04 18:04:02 -0400

    Refactor the code to put it inside function fetchBreedDescription

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,24 +1,34 @@
 const request = require('request');
-const args = process.argv;
-const breeds = ['Siberian', 'Bengal', 'Balinese','Abyssinian', 'Somali'];
-const id = ['sib','beng', 'bali','abys', 'soma'];
-let type = "";
-for (let i = 0; i < breeds.length; i++) {
-  if (args[2] === breeds[i]) {
-    type = id[i];
+
+const fetchBreedDescription = function(breed, callback) {
+  const breeds = ['Siberian', 'Bengal', 'Balinese','Abyssinian', 'Somali'];
+  const id = ['sib','beng', 'bali','abys', 'soma'];
+  let type = "";
+  for (let i = 0; i < breeds.length; i++) {
+    if (breed === breeds[i]) {
+      type = id[i];
+    }
   }
-}
 
-if (type === "") {
-  console.log("Breed not found");
-} else {
-  const web = "https://api.thecatapi.com/v1/breeds/search?q=" + type;
+  if (type === "") {
+    let error = "Breed not Found!";
+    callback(error, null);
+  } else {
 
-  request(web, function(error, response, body) {
-  //console.error('error:', error);
-  //console.log('statusCode:', response && response.statusCode);
-    const data = JSON.parse(body); // converting body part to object
-    console.log(typeof data);
-    console.log(data[0].description);
-  });
-}
+    const web = "https://api.thecatapi.com/v1/breeds/search?q=" + type;
+
+    request(web, function(error, response, body) {
+      if (error) {
+        callback(error, null);
+      }
+      //console.log('statusCode:', response && response.statusCode);
+      const data = JSON.parse(body); // converting body part to object
+      callback(null,data[0].description);
+
+    });
+  }
+};//End of Function
+
+
+module.exports = {fetchBreedDescription};
+
